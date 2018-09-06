7f0c0d95757e0f432f20f96234224b9235d11dfa
Qudrat <ibadullayev.qudrat@mail.ru>
2018-09-06 05:47:57 +0300

    Toolbar changed

--- app/src/main/java/com/example/sportnews/MainActivity.java
+++ app/src/main/java/com/example/sportnews/MainActivity.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
